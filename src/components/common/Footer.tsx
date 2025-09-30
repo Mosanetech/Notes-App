c4d4a7960a298c8145e9d348e205acbe4fbd2542
wisdomphiri408 <wisdomphiri408@gmail.com>
2025-09-30 21:16:04 +0200

    Modified Loading.tsx and Not found page

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -13,7 +13,7 @@ const Footer: NextPage = () => {
             {/*all rights*/}
             <div className={'w-full max-w-[1200px] mx-auto flex flex-col md:flex-row items-center justify-between border-t py-5'}>
                 <p className={'text-sm'}>© 2024 SchoolNotes. All rights reserved.</p>
-                <p className={'flex items-center'}>
+                <p className={'flex items-center gap-4'}>
                     <Link href={'/privacy-policy'} className={'text-xs'}>Privacy Policy</Link>
                     <Link href={'/terms-of-service'} className={'text-xs'}>Terms of service</Link>
                 </p>
